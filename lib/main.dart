46641055c42c43746cd6cbf64c101e2160bd2b23
MACBOOK <beratkaya1998@gmail.com>
2020-07-30 23:37:47 +0300

    Added basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:playify/playify.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Playify',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  SongInfo currentSong;
+  Playify playify = Playify();
+  bool playing = false;
+  bool changing = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    isPlaying().then((_) => fetchCurrentSong());
+  }
+
+  Future<void> fetchCurrentSong() async {
+    try {
+      var res = await playify.nowPlaying();
+      setState(() {
+        currentSong = res;
+      });
+    } catch (e) {
+      print(e);
+      setState(() {
+        currentSong = null;
+      });
+    }
+  }
+
+  Future<void> isPlaying() async {
+    try {
+      var res = await playify.isPlaying();
+      print(res);
+      setState(() {
+        playing = res;
+      });
+    } catch (e) {
+      print(e);
+      setState(() {
+        playing = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        padding: EdgeInsets.only(top: AppBar().preferredSize.height * 0.6),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 4),
+                child: Column(
+                  children: [
+                    if (currentSong != null)
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey[400],
+                            shape: BoxShape.rectangle,
+                            image: DecorationImage(image: currentSong.album.coverArt.image),
+                            borderRadius: BorderRadius.circular(8)),
+                        height: MediaQuery.of(context).size.height * 0.5,
+                        width: MediaQuery.of(context).size.height * 0.5,
+                      )
+                    else
+                      Container(
+                        decoration:
+                            BoxDecoration(color: Colors.grey[400], borderRadius: BorderRadius.circular(8)),
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        width: MediaQuery.of(context).size.width * 0.95,
+                      )
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Wrap(alignment: WrapAlignment.center, children: [
+                        Container(
+                            decoration: BoxDecoration(color: Colors.grey[400], shape: BoxShape.circle),
+                            padding: EdgeInsets.fromLTRB(12, 16, 16, 16),
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.black,
+                            )),
+                      ]),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: IgnorePointer(
+                        ignoring: changing,
+                        child: GestureDetector(
+                          onTap: () async {
+                            try {
+                              setState(() {
+                                changing = true;
+                              });
+                              if (playing) {
+                                await playify.pause();
+                              } else {
+                                await playify.play();
+                              }
+                              setState(() {
+                                playing = !playing;
+                                changing = false;
+                              });
+                            } catch (e) {
+                              print(e);
+                              setState(() {
+                                changing = false;
+                              });
+                            }
+                          },
+                          child: Container(
+                              decoration: BoxDecoration(color: Colors.grey[400], shape: BoxShape.circle),
+                              padding: EdgeInsets.all(16),
+                              child: Icon(!playing ? Icons.play_arrow : Icons.pause)),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                          decoration: BoxDecoration(color: Colors.grey[400], shape: BoxShape.circle),
+                          padding: EdgeInsets.fromLTRB(16, 16, 12, 16),
+                          child: Icon(Icons.arrow_forward_ios, color: Colors.black)),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
