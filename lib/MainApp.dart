9280b375b35f66dab66f28302a25e4e2e44d40b3
stelselim <stelselim@Stelselims-MacBook-Pro.local>
2020-07-31 17:58:16 +0300

    Bottom Nav Bar, Scaffold Bodies added

---
+++ lib/MainApp.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:playify_app/screens/home.dart';
+import 'package:playify_app/screens/profile.dart';
+
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int index = 0;
+
+  Widget mainAppBodySwitch(int _index) {
+    if (_index == 0)
+      return HomeScreen();
+    else if (_index == 1)
+      return HomeScreen();
+    else if (_index == 2)
+      return ProfilePage();
+    else
+      return HomeScreen();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: mainAppBodySwitch(index),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (currentIndex) => setState(() {
+          index = currentIndex;
+        }),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Home"),
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Stats"),
+            icon: Icon(
+              Icons.data_usage,
+            ),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Profile"),
+            icon: Icon(
+              Icons.account_circle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,162 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:playify/playify.dart';
+import 'package:playify_app/MainApp.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(PlayifyApp());
 }
 
-class MyApp extends StatelessWidget {
+class PlayifyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Playify',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  SongInfo currentSong;
-  Playify playify = Playify();
-  bool playing = false;
-  bool changing = false;
-
-  @override
-  void initState() {
-    super.initState();
-    isPlaying().then((_) => fetchCurrentSong());
-  }
-
-  Future<void> fetchCurrentSong() async {
-    try {
-      var res = await playify.nowPlaying();
-      setState(() {
-        currentSong = res;
-      });
-    } catch (e) {
-      print(e);
-      setState(() {
-        currentSong = null;
-      });
-    }
-  }
-
-  Future<void> isPlaying() async {
-    try {
-      var res = await playify.isPlaying();
-      print(res);
-      setState(() {
-        playing = res;
-      });
-    } catch (e) {
-      print(e);
-      setState(() {
-        playing = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: AppBar().preferredSize.height * 0.6),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 4),
-                child: Column(
-                  children: [
-                    if (currentSong != null)
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.grey[400],
-                            shape: BoxShape.rectangle,
-                            image: DecorationImage(image: currentSong.album.coverArt.image),
-                            borderRadius: BorderRadius.circular(8)),
-                        height: MediaQuery.of(context).size.height * 0.5,
-                        width: MediaQuery.of(context).size.height * 0.5,
-                      )
-                    else
-                      Container(
-                        decoration:
-                            BoxDecoration(color: Colors.grey[400], borderRadius: BorderRadius.circular(8)),
-                        height: MediaQuery.of(context).size.height * 0.4,
-                        width: MediaQuery.of(context).size.width * 0.95,
-                      )
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Wrap(alignment: WrapAlignment.center, children: [
-                        Container(
-                            decoration: BoxDecoration(color: Colors.grey[400], shape: BoxShape.circle),
-                            padding: EdgeInsets.fromLTRB(12, 16, 16, 16),
-                            child: Icon(
-                              Icons.arrow_back_ios,
-                              color: Colors.black,
-                            )),
-                      ]),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: IgnorePointer(
-                        ignoring: changing,
-                        child: GestureDetector(
-                          onTap: () async {
-                            try {
-                              setState(() {
-                                changing = true;
-                              });
-                              if (playing) {
-                                await playify.pause();
-                              } else {
-                                await playify.play();
-                              }
-                              setState(() {
-                                playing = !playing;
-                                changing = false;
-                              });
-                            } catch (e) {
-                              print(e);
-                              setState(() {
-                                changing = false;
-                              });
-                            }
-                          },
-                          child: Container(
-                              decoration: BoxDecoration(color: Colors.grey[400], shape: BoxShape.circle),
-                              padding: EdgeInsets.all(16),
-                              child: Icon(!playing ? Icons.play_arrow : Icons.pause)),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                          decoration: BoxDecoration(color: Colors.grey[400], shape: BoxShape.circle),
-                          padding: EdgeInsets.fromLTRB(16, 16, 12, 16),
-                          child: Icon(Icons.arrow_forward_ios, color: Colors.black)),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+      home: MainApp(),
     );
   }
 }
